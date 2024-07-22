8059677eb222a4c5c15133cf6c1fc74fd3a00b50
ltytgat <loantytgat@gmail.com>
2024-07-22 18:34:47 +0200

    Chapter 2 - New case

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ from main import Dollar
 
 def test_multiplication():
     five = Dollar(5)
-    five.times(2)
-    assert five.amount == 10
+    product = five.times(2)
+    assert product.amount == 10
+    product = five.times(3)
+    assert product.amount == 15
