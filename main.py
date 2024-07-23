6847413b67ce52782605763b5364ba45d4b43843
ltytgat <loantytgat@gmail.com>
2024-07-23 17:43:18 +0200

    Chapter 9 - Test ok - first step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Money:
     def __init__(self, value: int):
         self._amount = value
+        self._currency = ""
 
     def __eq__(self, other):
         if isinstance(other, type(self)):
@@ -15,12 +16,23 @@ class Money:
     def franc(amount: int):
         return Franc(amount)
 
+    def currency(self):
+        return self._currency
+
 
 class Dollar(Money):
+    def __init__(self, value: int, cur: str):
+        super().__init__(value)
+        self._currency = "USD"
+
     def times(self, multiplier: int):
         return Dollar(self._amount * multiplier)
 
 
 class Franc(Money):
+    def __init__(self, value: int):
+        super().__init__(value)
+        self._currency = "CHF"
+
     def times(self, multiplier: int):
         return Franc(self._amount * multiplier)
