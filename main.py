b6f8f0f00419126f58878c60834bc199a964aecf
ltytgat <loantytgat@gmail.com>
2024-07-24 17:35:17 +0200

    Chapter 11 - test Ok

--- main.py
+++ main.py
@@ -8,24 +8,14 @@ class Money:
 
     @staticmethod
     def dollar(amount: int):
-        return Dollar(amount, "USD")
+        return Money(amount, "USD")
 
     @staticmethod
     def franc(amount: int):
-        return Franc(amount, "CHF")
+        return Money(amount, "CHF")
 
     def currency(self):
         return self._currency
 
     def times(self, multiplier: int):
         return Money(self._amount * multiplier, self._currency)
-
-
-class Dollar(Money):
-    def __init__(self, value: int, cur: str):
-        super().__init__(value, cur)
-
-
-class Franc(Money):
-    def __init__(self, value: int, cur: str):
-        super().__init__(value, cur)
