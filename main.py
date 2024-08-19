a29dfae7f3a8bc16ef89eb456e6bdfb2698231fa
ltytgat <loantytgat@gmail.com>
2024-08-19 12:22:45 +0200

    Chapter 18 - Final refactor

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.was_run = None
         self.name = name
-
-    def test_method(self):
-        self.was_run = 1
 
     def run(self):
         method = getattr(self, self.name)
         method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        TestCase.__init__(self, name)
+
+    def test_method(self):
+        self.was_run = 1
+
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("test_method")
+        assert not test.was_run
+        test.run()
+        assert test.was_run
+
+
+TestCaseTest("test_running").run()
--- test.py
+++
@@ -1,6 +0,0 @@
-from main import WasRun
-
-test = WasRun("test_method")
-print(test.was_run)
-test.run()
-print(test.was_run)
