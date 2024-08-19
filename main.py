b5c22508b390050fa84605c9268adf0d54f7b4c9
ltytgat <loantytgat@gmail.com>
2024-08-19 12:10:03 +0200

    Chapter 18 - First step

---
+++ main.py
@@ -0,0 +1,6 @@
+class WasRun:
+    def __init__(self, name):
+        self.was_run = None
+
+    def test_method(self):
+        self.was_run = 1
---
+++ test.py
@@ -0,0 +1,6 @@
+from main import WasRun
+
+test = WasRun("test_method")
+print(test.was_run)
+test.test_method()
+print(test.was_run)
